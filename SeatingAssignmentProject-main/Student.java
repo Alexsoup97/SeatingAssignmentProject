1dacee423b0867b8f1b0cd81e6af92b7768a495f
Monkey-Masterr <91709318+Monkey-Masterr@users.noreply.github.com>
2021-10-09 14:11:23 -0400

    Add files via upload
    There are problems with the timer when you spam it, problems with deleting rows, and the try catch error aren't so good

---
+++ SeatingAssignmentProject-main/Student.java
@@ -0,0 +1,54 @@
+
+public class Student{
+
+
+    private String name;
+    private int id;
+    private String cohort;
+    //Student[] friendPreferences;
+
+
+    public Student(String name, int id, String cohort){
+
+        this.name = name;
+        this.id = id;
+        this.cohort = cohort;
+        //this.friendPreferences = friendPrefences;
+
+    }
+
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCohort() {
+        return this.cohort;
+    }
+
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+
+    // public Student[] getFriendPreferences() {
+    //     return this.friendPreferences;
+    // }
+
+    // public void setFriendPreferences(Student[] friendPreferences) {
+    //     this.friendPreferences = friendPreferences;
+    // }
+
+
+}
