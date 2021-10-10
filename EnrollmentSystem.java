d5de905cc420bae212d7f9703cf7d6c1510c9c31
Monkey-Masterr <91709318+Monkey-Masterr@users.noreply.github.com>
2021-10-10 17:44:07 -0400

    Add files via upload

--- EnrollmentSystem.java
+++ EnrollmentSystem.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.awt.Graphics;
-import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -8,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.InputMismatchException;
+import javax.swing.Timer;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,14 +17,19 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
-import javax.swing.JComboBox;
 
 public class EnrollmentSystem {
     private static ArrayList<Student> studentList = new ArrayList <Student>();
-      
     public EnrollmentSystem() {
         generateJTable();
     }
+
+    // private static boolean run;
+
+    // public void setVisible(boolean run){
+    //     this.run = run;
+    // }
+
 
     public static void generateJTable() {
 
@@ -54,32 +60,34 @@ public class EnrollmentSystem {
          JTextField textID = new JTextField();
          JTextField textCohort = new JTextField();
          JTextField textFriends = new JTextField();
+
         
         // create JButtons to add the action
         JButton btnAdd = new JButton("Add");
         JButton btnDelete = new JButton("Delete");
         JButton btnUpdate = new JButton("Update");
+        JButton btnBack = new JButton("Back");
+
 
         JLabel errorMessage = new JLabel();
+        errorMessage.setForeground (Color.red);
+        errorMessage.setFont(new Font("Calibri", Font.ITALIC, 25));
 
        // JComboBox<Student> friendPrefOne = new JComboBox<Student>();
 
 
-        errorMessage.setBounds(350, 250, 150, 25);
-  
-
-
-
-
+        errorMessage.setBounds(350, 250, 300, 25);
         
-        textName.setBounds(20, 220, 150, 25);
-        textID.setBounds(20, 250, 150, 25);
-        textCohort.setBounds(20, 280, 150, 25);
-        textFriends.setBounds(20, 310, 150, 25);
-        
-        btnAdd.setBounds(200, 220, 100, 25);
-        btnUpdate.setBounds(200, 265, 100, 25);
-        btnDelete.setBounds(200, 310, 100, 25);
+        textName.setBounds(20, 240, 150, 25);
+        textID.setBounds(20, 270, 150, 25);
+        textCohort.setBounds(20, 300, 150, 25);
+        textFriends.setBounds(20, 330, 150, 25);
+
+
+        btnBack.setBounds(10, 210, 100, 25);
+        btnAdd.setBounds(200, 240, 100, 25);
+        btnUpdate.setBounds(200, 285, 100, 25);
+        btnDelete.setBounds(200, 330, 100, 25);
         
         // create JScrollPane
         JScrollPane pane = new JScrollPane(table);
@@ -100,6 +108,7 @@ public class EnrollmentSystem {
         frame.add(btnAdd);
         frame.add(btnDelete);
         frame.add(btnUpdate);
+        frame.add(btnBack);
         
         // create an array of objects to set the row data
         Object[] data = new Object[4];
@@ -109,19 +118,40 @@ public class EnrollmentSystem {
         
         @Override
         public void actionPerformed(ActionEvent e) {
-        data[0] = textName.getText();
-        data[1] = Integer.valueOf(textID.getText());
-        data[2] = textCohort.getText();
-        data[3] = textFriends.getText();
+        
+            try{
+                data[0] = textName.getText();
+                data[1] = Integer.valueOf(textID.getText());
+                data[2] = textCohort.getText();
+                data[3] = textFriends.getText();
+            }catch(NumberFormatException a){
+                // data[0]=null;
+                // data[1]=null;
+                // data[2]=null;
+                // data[3]=null;
+  
+                for (int i=0; i <4; i++){
+                    data[i]= null;
+                }
+                // for(Object objects: data){
+                //     objects == null;
+                // }   
+
+                errorMessage.setText("Inccorect Inputs");
+                clearErrorMessage(errorMessage);
+            }
+
         textName.setText("");
         textID.setText("");
         textCohort.setText("");
         textFriends.setText("");
         addStudent((String)data[0],(int)data[1], (String)data[2]);
         model.addRow(data);
+
+
         }
         });
-        
+
         // button remove row - Clicked on Delete Button
         btnDelete.addActionListener(new ActionListener() {
         
@@ -130,12 +160,11 @@ public class EnrollmentSystem {
         
         // i = the index of the selected row
         int i = table.getSelectedRow();
-  
-
 
         if (i >= 0) {
         // remove a row from jtable
-        removeStudent((int)model.getValueAt(i,1));
+        //removeStudent((int)model.getValueAt(i,1));
+        removeStudent(i);
         model.removeRow(i);
         textName.setText("");
         textID.setText("");
@@ -143,6 +172,8 @@ public class EnrollmentSystem {
         textFriends.setText("");
         } else {
         errorMessage.setText("Please select a valid row");
+        clearErrorMessage(errorMessage);
+
         }
         }
         });
@@ -175,15 +206,42 @@ public class EnrollmentSystem {
         int i = table.getSelectedRow();
         
         if (i >= 0) {
-        model.setValueAt(textName.getText(), i, 0);
-        model.setValueAt(textID.getText(), i, 1);
-        model.setValueAt(textCohort.getText(), i, 2);
-        model.setValueAt(textFriends.getText(), i, 3);
+            try{
+
+                data[0] = textName.getText();
+                data[1] = Integer.valueOf(textID.getText());
+                data[2] = textCohort.getText();
+                data[3] = textFriends.getText();
+
+                model.setValueAt(data[0], i, 0);
+                model.setValueAt(data[1], i, 1);
+                model.setValueAt(data[2], i, 2);
+                model.setValueAt(data[3], i, 3);
+
+
+            }catch(NumberFormatException a){
+                errorMessage.setText("Inccorect Inputs");
+                clearErrorMessage(errorMessage);
+            }
+
+         updateStudent(i,(String)data[0],(int)data[1], (String)data[2]);
+
         } else {
-        System.out.println("Update Error");
+            errorMessage.setText("Please select a valid row");
+            clearErrorMessage(errorMessage);
+            
         }
         }
         });
+
+         btnBack.addActionListener(new ActionListener() {
+        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new SystemManager();
+            }
+            });
         
         frame.setSize(900, 400);
         frame.setLocationRelativeTo(null);
@@ -198,16 +256,36 @@ public class EnrollmentSystem {
 
     }
 
-    public static void removeStudent(int id) {
 
-        for(int i = 0; i < studentList.size(); i++){
-            if(studentList.get(i).getId() == id){
-                studentList.remove(i);
-            } 
-        }
+    public static void removeStudent(int i) {
+
+        studentList.remove(i);
+
+        // for(int i = 0; i < studentList.size(); i++){
+        //     if(studentList.get(i).getId() == id){
+        //         studentList.remove(i);
+        //     } 
+        // }
+
+    }
+
+    public static void updateStudent(int i, String name, int id, String cohort) {
+        studentList.get(i).setName(name);
+        studentList.get(i).setID(id);
+        studentList.get(i).setCohort(cohort);
+    }
 
 
+    public static void clearErrorMessage(JLabel errorMessage){
 
+        //if you spam update error, the timer doesnt work
+        Timer timer = new Timer (2500, new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                errorMessage.setText("");
+            }
+        });
+        timer.start();
+        
     }
 
     public  static void displayStudents() {
