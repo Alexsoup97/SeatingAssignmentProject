9174fd85c991b495e58734284a5ed5e28ea38fe9
Alexsoup97 <43156040+Alexsoup97@users.noreply.github.com>
2021-10-15 21:07:49 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * [Main.java]
+ * Runs the entire program
+ * @author Alex, Nicholas, Samson
+ * @version 1.0 Oct 14, 2021
+**/
+public class Main {
+
+    public static void main(String[] args){
+
+        SystemManager sysManager = new SystemManager();
+        sysManager.drawMenu();
+
+    }
+}
